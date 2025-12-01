f4b86c5bee51529481fbe34b0b0cf5279dda40f6
mello-stefani <211365@upf.br>
2025-11-30 21:26:07 -0300

    Delete peça 4 notas bimestrais e mostre a média.py

--- peça 4 notas bimestrais e mostre a média.py
+++
@@ -1,8 +0,0 @@
-nota_1 = float(input("Digite a nota1: "))
-nota_2 = float(input("Digite a nota2: "))
-nota_3 = float(input("Digite a nota3: "))
-nota_4 = float(input("Digite a nota4: "))
-
-média = (nota_1 + nota_2 + nota_3 + nota_4) / 4
-
-print("A média é:", média)
